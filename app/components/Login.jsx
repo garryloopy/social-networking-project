49d7aa0191406bb42c85e2c782065c2940531b1a
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-02 14:15:11 -0700

    Added login component

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,24 +1,28 @@
 "use client";
 
 import { useUserAuth } from "../_utils/auth-context";
+import Subheading from "./texts/Subheading";
 
 export default function Login( {onClick} ) {
-    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+    const { user, gitHubSignIn} = useUserAuth();
 
     const handleOnButtonClick = async () => {
         await gitHubSignIn();
-        onClick();
+
+        if (onClick)
+            onClick();
     }
     
     return (
-        <section className="bg-slate-500 p-4 flex justify-between gap-5 border-2 rounded-3xl hover:bg-slate-600 hover:text-white active cursor-pointer active:bg-slate-500"
+        <section className="bg-[#606C38] p-4 flex justify-between gap-5 border-2 rounded-3xl hover:bg-[#283618] hover:text-white active cursor-pointer active:bg-[#606C38]"
                  onClick={handleOnButtonClick}>
             <img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" 
                  alt="Github logo" 
                  width={100}
                  height={100}
                  className="rounded-3xl flex-0"/>
-            <p className="mt-auto mb-auto flex-1 text-4xl text-gray-100">
+            
+            <p className="mt-auto mb-auto flex-1 text-gray-100 font-medium text-3xl">
                 Login with GitHub
             </p>
         </section>
