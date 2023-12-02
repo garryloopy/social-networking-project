2ffe44451cea33286418eae76e7373cd180c1d8a
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-02 14:16:10 -0700

    Minor change is main page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
+"use client";
+
+import LoginPage from "./pages/LoginPage";
 import LandingPage from "./pages/LandingPage";
 
+import {
+  useUserAuth
+} from "./_utils/auth-context"
+
 export default function Page() {
+  const { user } = useUserAuth();
   return (
-    <LandingPage />
+    <div>
+      {!user &&
+        <LoginPage />
+      }
+      {user &&
+        <LandingPage />
+      }
+    </div>
   )
 }
