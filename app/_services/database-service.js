4e49bb32864a25cc42e388b179291ba2ad9e1209
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-02 14:15:47 -0700

    Added database logic

--- app/_services/database-service.js
+++ app/_services/database-service.js
@@ -1,6 +1,48 @@
 import { db } from "../_utils/firebase";
 import { collection, getDocs, addDoc } from "firebase/firestore";
 
+export const createPost = async ({post}) => {
+    try {
+        const docRef = await addDoc(collection(db, "users"), {
+            userId: post.uid,
+            photoURL: post.photoURL,
+            displayName: post.displayName,
+            timePosted: post.timePosted,
+            contents: post.contents
+        });
+
+        return docRef.id;
+    } catch (error) {
+        console.error("Error creating user:", error);
+        throw error;
+    }
+}
+
+export const getAllPosts = async () => {
+    try {
+        const q = collection(db, "posts");
+        const querySnapshot = await getDocs(q);
+
+        let posts = [];
+
+        querySnapshot.forEach(
+            (doc) => { 
+                posts.push(
+                    {
+                        docId: doc.id,
+                        data: doc.data()
+                    }
+                )
+            }
+        );
+
+        return posts;
+    } catch (error) {
+        console.error("Error getting posts:", error);
+        throw error;
+    }
+}
+
 export const getAllUsers = async () => {
     try {
         const q = collection(db, "users");
