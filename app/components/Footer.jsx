6627deadb0bae35b31f90056d76d0a70a55b3377
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-04 17:16:06 -0700

    Create Footer.jsx

---
+++ app/components/Footer.jsx
@@ -0,0 +1,9 @@
+import Subtext from "./texts/Subtext"
+
+export default function Footer() {
+    return (
+        <footer className="mb-0 mt-auto text-center">
+            <Subtext>You've reached the end of the page.</Subtext>
+        </footer>
+    )
+}
