b4b0451d57c6bdd493832ce48e508614bbe969cf
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-04 17:16:13 -0700

    Update Continue.jsx

--- app/components/Continue.jsx
+++ app/components/Continue.jsx
@@ -9,12 +9,12 @@ export default function Continue( {onClick} ) {
     }
     
     return (
-        <Link className="bg-[#606C38] p-4 flex justify-between gap-5 border-2 rounded-3xl hover:bg-[#283618] hover:text-white active cursor-pointer active:bg-[#606C38]"
+        <Link className="bg-gray-50 p-4 flex justify-between gap-5 rounded-sm cursor-pointer hover:bg-white active:bg-gray-50"
               onClick={handleOnButtonClick}
-              href="../pages/LandingPage">
+              href="../pages/Home">
             
-            <p className="mt-auto mb-auto flex-1 text-gray-100 font-medium text-3xl text-center">
-                Continue to the page
+            <p className="mt-auto mb-auto flex-1 text-black font-medium text-3xl text-center">
+                Continue
             </p>
         </Link>
     )
