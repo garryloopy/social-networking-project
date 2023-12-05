03672487676fef5e586d34ffe8b46f4ffb99515c
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-05 13:02:07 -0700

    Update page.js

--- app/pages/Home/page.js
+++ app/pages/Home/page.js
@@ -7,7 +7,7 @@ import Footer from "@/app/components/Footer";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col gap-4">
-      <Header/>
+      <Header />
 
       <Main />
 
