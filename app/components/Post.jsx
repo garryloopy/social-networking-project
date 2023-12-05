2defc1087c097a8d5f1ecc9d816a7c908fdad64c
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-05 13:19:55 -0700

    Fixed bug regarding clicking on post

--- app/components/Post.jsx
+++ app/components/Post.jsx
@@ -25,7 +25,7 @@ export default function Post( {post} ) {
     return (
         <section className={`flex flex-col bg-gray-50 px-4 pt-4 rounded-sm ${!hidden ? "gap-4 pb-4" : ""} shadow-2xl` }>
             <div className={`flex flex-row justify-between ${!hidden ? "border-b-2" : ""} pb-4`}>
-                <Link href={`users/${post.data.userDocId}`}>
+                <Link href={`/pages/users/${post.data.userDocId}`}>
                     <div className="flex flex-row gap-4 p-2 px-4 bg-gray-300 hover:bg-gray-400 active:bg-gray-300 rounded-sm hover:cursor-pointer shadow-md">
                         <img src={post.data.photoURL} 
                                 alt="User image" 
