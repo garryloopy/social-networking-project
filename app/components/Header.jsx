8c67adb83214519125bfd0707aadda55f8606424
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-04 18:50:56 -0700

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -23,7 +23,7 @@ export default function header() {
     
         return (
           <button
-            className={`text-black ${subtextSize} ${subtextWeight} px-10 py-2 shadow-lg active:bg-gray-50 rounded-sm hover:bg-white bg-gray-50`}
+            className={`text-black ${subtextSize} ${subtextWeight} px-10 py-2 shadow-lg active:bg-gray-50 rounded-sm hover:bg-gray-200 bg-gray-50`}
             onClick={handleOnClick}
           >
             {children}
@@ -33,7 +33,9 @@ export default function header() {
 
     return (
         <header className="flex justify-between bg-gray-400 border-b-2 border-gray-50 pb-2 px-24 pt-2">
-            <NavBarButton>Home</NavBarButton>
+            <Link href={"../Home"}>
+              <NavBarButton>Home</NavBarButton>
+            </Link>
             <div className="flex flex-row gap-5">
             <NavBarButton>Settings</NavBarButton>
             <Link href="/">
