aca335a9405f6cc17b7f8a766d7398f510b3f2a6
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-04 18:51:10 -0700

    Update User.jsx

--- app/components/User.jsx
+++ app/components/User.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+
 import Text from "./texts/Text";
 import Subtext from "./texts/Subtext";
 
@@ -13,21 +15,26 @@ export default function User( {user, onUserHide} ) {
     const handleOnUserHide = () => {
         
     }
+    const debug = () => {
+        console.log(user);
+    }
 
     return (
         <section className="flex flex-col bg-gray-50 p-4 rounded-sm gap-4 shadow-2xl">
             <div className="flex flex-row justify-between">
-                <div className="flex flex-row gap-4 p-2 px-4 bg-gray-300 hover:bg-gray-400 active:bg-gray-300 rounded-sm hover:cursor-pointer shadow-md">
-                    <img src={user.userInfo.photoURL} 
-                            alt="User image" 
-                            width={60}
-                            height={60}
-                            className="rounded-full border-2 border-gray-500"/>
-                    <div className="my-auto">
-                        <Text>{user.userInfo.displayName}</Text>
-                        <Subtext>{user.userInfo.bio}</Subtext>
+                <Link href={`users/${user.docId}`}>
+                    <div className="flex flex-row gap-4 p-2 px-4 bg-gray-300 hover:bg-gray-400 active:bg-gray-300 rounded-sm hover:cursor-pointer shadow-md" onClick={debug}>
+                        <img src={user.userInfo.photoURL} 
+                                alt="User image" 
+                                width={60}
+                                height={60}
+                                className="rounded-full border-2 border-gray-500"/>
+                        <div className="my-auto">
+                            <Text>{user.userInfo.displayName}</Text>
+                            <Subtext>{user.userInfo.bio}</Subtext>
+                        </div>
                     </div>
-                </div>
+                </Link>
             </div>
         </section>
     );
