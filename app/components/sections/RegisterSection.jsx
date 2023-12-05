8b1130e2b0d85524fcfab7ceb645ce39b301f360
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-04 17:15:38 -0700

    Create RegisterSection.jsx

---
+++ app/components/sections/RegisterSection.jsx
@@ -0,0 +1,11 @@
+import Subheading from "../texts/Subheading";
+import Register from "../Register";
+
+export default function RegisterSection ({user, onRegister}) {
+    return (
+      <div className="flex flex-col gap-2">
+        <Subheading>It seems like you're new here...</Subheading>
+        <Register user={user} onRegister={onRegister}/>
+      </div>
+    );
+  }
