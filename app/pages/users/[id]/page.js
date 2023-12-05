cdb41112a736a0c54bd360ab3b1940386b262a55
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-04 18:51:12 -0700

    Create page.js

---
+++ app/pages/users/[id]/page.js
@@ -0,0 +1,16 @@
+"use client";
+
+import Header from "@/app/components/Header";
+import Profile from "@/app/components/Profile";
+import Footer from "@/app/components/Footer";
+
+export default function UserPage({params}) {
+  return (
+    <div className="min-h-screen flex flex-col gap-4">
+      <Header/>
+      <Profile docId={params.id} />
+
+      <Footer />
+    </div>
+  );
+}
