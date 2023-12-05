4f74b24464a454ac10acd4c59c54726fa51cba77
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-04 17:16:20 -0700

    Create page.js

---
+++ app/pages/Home/page.js
@@ -0,0 +1,17 @@
+"use client";
+
+import Header from "@/app/components/Header";
+import Main from "@/app/components/Main";
+import Footer from "@/app/components/Footer";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col gap-4">
+      <Header/>
+
+      <Main />
+
+      <Footer />
+    </div>
+  );
+}
