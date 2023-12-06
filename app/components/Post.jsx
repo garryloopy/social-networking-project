7ec25d92811deca5c30ff052bfa5e60b07f5624c
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-06 16:40:55 -0700

    Added Icon

--- app/components/Post.jsx
+++ app/components/Post.jsx
@@ -38,20 +38,15 @@ export default function Post( {post} ) {
                         </div>
                     </div>
                 </Link>
-                <img src={"https://cdn-icons-png.flaticon.com/128/565/565655.png"} 
+                
+                <div className="my-auto">
+
+                <img src={hidden ? "https://cdn-icons-png.flaticon.com/128/4298/4298899.png" : "https://cdn-icons-png.flaticon.com/128/565/565655.png"} 
                                 alt="User image" 
                                 width={100}
                                 height={100}
-                                className="rounded-full border-2 border-gray-500"/>
-                {/* https://cdn-icons-png.flaticon.com/128/565/565655.png */}
-                <div className="my-auto">
-                    <button className={`text-black ${textSize} ${textWeight} bg-gray-300 py-6 px-7 rounded-sm hover:bg-gray-400 active:bg-gray-300 shadow-md`}
-                            onClick={handleOnPostHide}>
-                        {
-                            hidden ? "Show"
-                            : "Hide"
-                        }
-                    </button>
+                                className={`text-black ${textSize} ${textWeight} bg-gray-300 py-6 px-7 rounded-xl hover:bg-gray-400 active:bg-gray-300 shadow-md`}
+                                onClick={handleOnPostHide}/>
                 </div>
             </div>
             <div>
