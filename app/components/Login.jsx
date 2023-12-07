445039d4ed1dd397391fe2bbebd7507d7ee0826a
Edward Montilla <edward.montilla@edu.sait.ca>
2023-12-06 18:20:49 -0700

    stylez

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -15,15 +15,14 @@ export default function Login( {onClick} ) {
     }
     
     return (
-        <section className="bg-gray-400 p-2 flex justify-between gap-5 rounded-sm
-                            hover:bg-gray-500 hover:text-white active cursor-pointer 
-                            active:bg-gray-400 shadow-2xl"
+        <section className="bg-gray-50 p-2 flex justify-between gap-5 rounded-md
+                            hover:bg-gray-200 hover:text-white active cursor-pointer shadow-2xl"
                  onClick={handleOnButtonClick}>
             <img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" 
                  alt="Github logo" 
                  width={60}
                  height={60}
-                 className="rounded-sm m-1"/>
+                 className="rounded-sm m-1 hover:bg-gray-200"/>
             
             <div className="mt-auto mb-auto flex-1">
                 <Subheading>Login with GitHub</Subheading>
