e72efa6cb29c070da85c6fc49e93981c80200138
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-12-06 18:20:06 -0700

    Centered back to login

--- app/pages/users/[id]/page.js
+++ app/pages/users/[id]/page.js
@@ -4,6 +4,8 @@ import Header from "@/app/components/Header";
 import Profile from "@/app/components/Profile";
 import Footer from "@/app/components/Footer";
 
+
+
 export default function UserPage({params}) {
   return (
     <div className="min-h-screen flex flex-col gap-4">
